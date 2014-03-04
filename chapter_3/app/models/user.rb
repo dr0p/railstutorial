ea7aaf299b48082c1a0d43f28a4903a9e1b706e7
Paweł Drożański <pawdro@gmail.com>
2014-03-04 12:48:14 +0100

    Make a basic User model (including secure passwords)

--- chapter_3/app/models/user.rb
+++ chapter_3/app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
+  validates_confirmation_of :password,
+    if: lambda { |m| m.password.present? }
+
+  has_secure_password
 end
