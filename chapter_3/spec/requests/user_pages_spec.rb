9bed740ec60bcfbf816ea31a81a212b395405baa
Paweł Drożański <pawdro@gmail.com>
2014-03-05 11:32:34 +0100

    Finish user signup

--- chapter_3/spec/requests/user_pages_spec.rb
+++ chapter_3/spec/requests/user_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "User Pages" do
+
   subject { page }
   
   describe "signup page" do
@@ -8,6 +9,14 @@ describe "User Pages" do
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name)}
   end
 
 end
